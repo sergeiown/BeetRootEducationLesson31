b803c940a471a771151617a79628deea0108f066
Serhii Myshko <sergeiown@gmail.com>
2022-12-11 12:38:20 +0200

    continue work

--- js/minimum.js
+++ js/minimum.js
@@ -1,1 +1,17 @@
-export {};
+function graduateUserAge(userAge) {}
+
+function displaySymbol(userKey) {}
+
+function addupNumbers(userRangeStart, userRangeEnd) {}
+
+function findCommonDivisor(userFirstNumber, userSecondNumber) {}
+
+function findAllDevisors(userNumber) {}
+
+export {
+  graduateUserAge,
+  displaySymbol,
+  addupNumbers,
+  findCommonDivisor,
+  findAllDevisors,
+};
