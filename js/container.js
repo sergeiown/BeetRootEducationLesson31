9b27b57ad119d9303941b962ac3dfcf4ba5a94b5
Serhii Myshko <sergeiown@gmail.com>
2022-12-11 14:53:59 +0200

    add user age graduator

--- js/container.js
+++ js/container.js
@@ -1,3 +1,13 @@
 import * as minimum from "./minimum.js";
-import * as normal from "./normal.js";
-import * as maximum from "./maximum.js";
+// import * as normal from "./normal.js";
+// import * as maximum from "./maximum.js";
+
+confirm;
+const userAge = minimum.graduateUserAge(prompt("What is your age?", 100));
+alert(`You are ${userAge}`);
+
+function closeTab() {
+  if (confirm("Бажаєте закінчити?")) {
+    window.close();
+  }
+}
--- js/minimum.js
+++ js/minimum.js
@@ -1,6 +1,49 @@
-function graduateUserAge(userAge) {}
+function graduateUserAge(userAge) {
+  let resaultGraduation =
+    userAge < 1
+      ? "false data submitter!"
+      : userAge < 12
+      ? "a child."
+      : userAge < 18
+      ? "a teenager."
+      : userAge < 60
+      ? "an adult."
+      : userAge < 123
+      ? "a pensioner."
+      : userAge > 122
+      ? "false data submitter!"
+      : "false data submitter!";
 
-function displaySymbol(userKey) {}
+  // let resaultGraduation = "";
+  // if (userAge < 1) {
+  //   resaultGraduation = "false data submitter!";
+  // } else if (userAge < 12) {
+  //   resaultGraduation = "a child.";
+  // } else if (userAge < 18) {
+  //   resaultGraduation = "a teenager.";
+  // } else if (userAge < 60) {
+  //   resaultGraduation = "an adult.";
+  // } else if (userAge < 123) {
+  //   resaultGraduation = "a pensioner.";
+  // } else if (userAge > 122) {
+  //   resaultGraduation = "false data submitter!";
+  // } else {
+  //   resaultGraduation = "false data submitter!";
+  // }
+
+  return resaultGraduation;
+}
+
+function displaySymbol(userKey) {
+  let resultSymbol = "";
+
+  if (resultSymbol >= 0 && resultSymbol <= 9) {
+  } else {
+    resultSymbol = "you had to enter a number from 0 to 9";
+  }
+
+  return resultSymbol;
+}
 
 function addupNumbers(userRangeStart, userRangeEnd) {}
 
